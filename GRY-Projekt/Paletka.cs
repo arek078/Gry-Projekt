c8e62355e2d9dd9ef27cd3f91cf12620cf32d808
Arek Szwarc <arek078@gmail.com>
2017-05-30 13:08:52 +0200

    funkcja losowego kolorowania plytek

--- GRY-Projekt/Paletka.cs
+++ GRY-Projekt/Paletka.cs
@@ -12,9 +12,27 @@ namespace GRY_Projekt
 {
     public partial class Paletka : Form
     {
+        bool wPrawo;
+        bool wLewo;
+        int predkosc = 10;
+
+        int pilkax = 3;
+        int pilkay = 3;
+
+        int punkty = 0;
+
+        private Random rnd = new Random();
         public Paletka()
         {
             InitializeComponent();
+            foreach (Control x in this.Controls)
+            {
+                if (x.Tag == "plytka" && x is PictureBox )
+                {
+                    Color randomColor = Color.FromArgb(rnd.Next(256), rnd.Next(256), rnd.Next(256));
+                    x.BackColor = randomColor;
+                }
+            }
         }
     }
 }
