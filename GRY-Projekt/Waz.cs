509b258910b08dd3de79d2a49f0c5c8aabe0fb9c
Arek Szwarc <arek078@gmail.com>
2017-06-10 17:01:29 +0200

    Dodanie pliku ruch , startgry

--- GRY-Projekt/Waz.cs
+++ GRY-Projekt/Waz.cs
@@ -12,9 +12,31 @@ namespace GRY_Projekt
 {
     public partial class Waz : Form
     {
+        List<Punkt> waz = new List<Punkt>();
+        const int szerokosc = 16;
+        const int wysoksc = 16;
+        bool gameover = false;
+        int punkty = 0;
+        Punkt czesci_jedzenia = new Punkt();
+
         public Waz()
         {
             InitializeComponent();
+            StartGry();
         }
+
+        private void StartGry()
+        {
+            gameover = false;
+            punkty = 0;
+            waz.Clear();
+            Punkt glowa = new Punkt();
+            glowa.X = 1;
+            glowa.Y = 5;
+            waz.Add(glowa);
+        }
+
+
+        
     }
 }
